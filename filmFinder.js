965782d27d9d947fd2723617e4d340a36939d522
AKDK10 <adjankla@gmail.com>
2024-09-24 11:49:38 +0100

    add html and css file

--- filmFinder.js
+++ filmFinder.js
@@ -1,20 +1,19 @@
-const tmdbKey = '';
+const tmdbKey = 'c8d0fc24318970cd55eb8d0f564733d0';
 const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 //const playBtn = document.getElementById('playBtn');
 
 const getGenres = async () => {
-  const genreRequestEndpoint = 'genre/movie/list';
+  const genreRequestEndpoint = '/genre/movie/list';
   const requestParams = `?api_key=${tmdbKey}`;
   const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
 
   try{
     const response = await fetch(urlToFetch);
     if(response.ok){
-        console.log(response)
       const jsonResponse = await response.json();
-      console.log(jsonResponse);
-      return jsonResponse;
-    
+      console.log(jsonResponse)
+      const genres = jsonResponse.genres;
+      return genres;
      }
      else {
         console.log('Request failed with status:', response.status)
